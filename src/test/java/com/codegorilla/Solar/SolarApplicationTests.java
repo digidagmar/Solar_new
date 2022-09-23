ef8f0ef6dec0541e81c73f5e03f055ca15c7b1ed
Dagmar Hofman (Codegorilla) <dagmar@codegorilla.nl>
2022-09-23 20:27:43 +0200

    Initial Commit
    Basic Spring Boot REST API

---
+++ src/test/java/com/codegorilla/Solar/SolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegorilla.Solar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
